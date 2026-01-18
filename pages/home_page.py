1cdb26db845f6c30521b81409b1aa677af0c04c4
Pooh122000 <snehapoojary12022000@gmail.com>
2026-01-19 03:41:42 +0530

    Fix missing Page import in HomePage

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,7 +3,7 @@ Page Object Model: Home Page
 Description: Page object for DemoQA homepage
 """
 
-from playwright.sync_api import expect
+from playwright.sync_api import Page,expect
 
 
 class HomePage:
